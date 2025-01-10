07ea597b15bc8d2d7c5863ad9047b60e0ac1bf3b
manishimwe Isabella <manishimweisabella@gmail.com>
2025-01-10 15:49:51 +0200

    setup

--- src/Components/Add.jsx
+++ src/Components/Add.jsx
@@ -4,7 +4,7 @@ const Add = () => {
   return (
 
 
-    <div className='bg-[url(public/grade.png)] bg-cover bg-no-repeat h-screen flex justify-end flex-col pb-40 pl-20'>
+    <div className='bg-[url(/public/grade.png)] bg-cover bg-no-repeat h-screen flex justify-end flex-col pb-40 pl-20'>
       <h1 className='text-white font-bold mb-5 text-3xl'>STARSHIP TO ADD NASA ASTRONAUTS ON </h1>
         <h2 className='text-white font-bold  mb-2 text-3xl'>THE MOON</h2>
       <button className=" border-2  w-40 h-8  text-white font-bold mx-10">REWATCH</button>
