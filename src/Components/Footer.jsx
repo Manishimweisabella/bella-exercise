189502da145a239a756195f7b14aa7416d680191
manishimwe Isabella <manishimweisabella@gmail.com>
2025-01-10 17:11:11 +0200

    this footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className=' bg-slate-900 w-full py-40  text-white'>
-     <ul className='flex  justify-center gap-4'>
-    <li>Twitter</li>
+     <ul className='flex  justify-center gap-4 font-bold'> 
+    <li>SPACEX © 2022</li>
+     <li>Twitter</li>
+     <li>instagram</li>
     <li>Youtube</li>
-    <li>Linked</li>
-    <li>Flicon</li>
+    <li>Linkedin</li>
+    <li>Flickr</li>
     <li>Privicy</li>
     <li>Supplier</li>
 
